162b26554ea8b6f53512ea28a218445a12d24823
Fabricio1805 <fabricio.ss09@outlook.com>
2024-01-09 21:40:17 -0300

    adicionando o projeto spring boot no docker

--- backend-product-api/api-product/src/main/java/com/product/api/ApiProductApplication.java
+++ backend-product-api/api-product/src/main/java/com/product/api/ApiProductApplication.java
@@ -1,4 +1,4 @@
-package com.product.apiproduct;
+package com.product.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +8,6 @@ public class ApiProductApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiProductApplication.class, args);
+		System.out.println("Hello, World!");
 	}
-
 }
